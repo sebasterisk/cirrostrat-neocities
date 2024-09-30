d472cc87c445c7e9d04c3a18cdab53b6eb432237
seb rugas <snrugas2027@loyolablakefield.org>
2024-09-30 15:07:51 -0400

    21 or bust more things

--- creations/21orbust/game.js
+++ creations/21orbust/game.js
@@ -9,13 +9,21 @@ let bet = 0
 let valueofcards = 0
 let valueofcards_ace11 = 0
 
+let dealervalueofcards = 0
+let dealervalueofcards_ace11 = 0
+
 const suits = ["&hearts;", "&diams;", "&spades;", "&clubs;"]
 const truevalues = [1,2,3,4,5,6,7,8,9,10,10,10,10]
 const displayvalues = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 const dealer_disp = document.getElementById("dealer")
 const player_disp = document.getElementById("player")
 const cardval = document.getElementById("cardval")
+const dealercardval = document.getElementById("dealercardval")
+
 const hitbtn = document.getElementById("hit")
+const standbtn = document.getElementById("stand")
+const againbtn = document.getElementById("again")
+
 // fisher-yates solution from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array) {
     let currentIndex = array.length;
@@ -44,6 +52,8 @@ function newdeck(){
 }
 
 function deal(){
+    dealer_hand = []
+    player_hand = []
     for(let i=0; i<2; i++){
         let temp = deck.pop()
         dealer_hand.push(temp)
@@ -53,19 +63,26 @@ function deal(){
         player_hand.push(temp)
     }
 }
-function listhand(){
+function listhand(dealer_reveal){
     valueofcards = 0
+    dealervalueofcards = 0
     dealer_disp.innerHTML = ""
     player_disp.innerHTML = ""
     for(let i=0; i<dealer_hand.length; i++){
         let div = document.createElement("div")
         div.classList.add("card")
-        if(i==0){
+        if(i==0 && !dealer_reveal){
             div.classList.add("covered")
         } else {
-            div.innerHTML=`${displayvalues[dealer_hand[i].val - 1]}${suits[player_hand[i].suit]}`
+            div.innerHTML=`${displayvalues[dealer_hand[i].val - 1]}${suits[dealer_hand[i].suit]}`
+            dealervalueofcards += dealer_hand[i].true
         }
         dealer_disp.appendChild(div)
+    }
+    if (!dealer_reveal){
+        dealercardval.innerHTML = dealervalueofcards + " + ?"
+    }else{
+        dealercardval.innerHTML = dealervalueofcards
     }
 
     for(let i=0; i<player_hand.length; i++){
@@ -77,21 +94,55 @@ function listhand(){
     }
     cardval.innerHTML = valueofcards
 }
+function bust(){
+    cardval.innerHTML += " BUSTED!!!!"
+    againbtn.disabled = false
+    hitbtn.disabled = true
+    standbtn.disabled = true
+}
 function hit(){
     let temp = deck.pop()
     player_hand.push(temp)
     listhand()
+
+    if (valueofcards > 21){
+        bust()
+    }
+}
+
+function stand(){
+    listhand(true)
+    while (dealervalueofcards < 17){
+        let temp = deck.pop()
+        dealer_hand.push(temp)
+        listhand(true)
+    }
+    againbtn.disabled = false
+    hitbtn.disabled = true
+    standbtn.disabled = true
 }
 function init(){
     if(deck.length < 20){
         newdeck()
     }
     deal()
-    listhand()
+    listhand(false)
     console.log(dealer_hand, player_hand, deck.length)
+
+    againbtn.disabled = true
+    hitbtn.disabled = false
+    standbtn.disabled = false
 }
 init()
 
 hitbtn.addEventListener("click", ()=>{
     hit()
+})
+
+standbtn.addEventListener("click", ()=>{
+    stand()
+})
+
+againbtn.addEventListener("click", ()=>{
+    init()
 })
